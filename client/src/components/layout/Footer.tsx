fa7d8bceca6b7719fd7d6138faf60173b3734de9
peedrooxd <41061116-peedrooxd@users.noreply.replit.com>
2025-04-18 12:56:38 +0000

    Restored to 'afb920ae1f9b85d93be06f7ad8af2aa36ecc3f14'
    Replit-Restored-To: afb920ae1f9b85d93be06f7ad8af2aa36ecc3f14

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
                 </Link>
               </li>
               <li>
-                <Link href="/#produtos" className="text-gray-400 hover:text-white transition flex items-center">
+                <Link href="/" className="text-gray-400 hover:text-white transition flex items-center">
                   <ChevronRight className="h-4 w-4 mr-2" /> Produtos
                 </Link>
               </li>
